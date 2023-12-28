d4c16dc7dd3c0e5097a43512ecc9a4280ef17420
CristiStroescu <stroescucristian478@yahoo.com>
2023-12-28 13:02:58 +0200

    Created the constants file

--- fe_time_travellers_gazette/src/components/common/footer/Footer.jsx
+++ fe_time_travellers_gazette/src/components/common/footer/Footer.jsx
@@ -1,11 +1,15 @@
 /* eslint-disable react/jsx-no-target-blank */
 import InstagramIcon from "@mui/icons-material/Instagram";
 import TwitterIcon from "@mui/icons-material/Twitter";
+import { TITLE } from "../../../constants/constants";
 
 function Footer() {
+  const title = TITLE;
   return (
     <footer>
-      <p>&copy; {new Date().getFullYear()} Time Traveller&apos;s Gazette</p>
+      <p>
+        &copy; {new Date().getFullYear()} {title}
+      </p>
       <a
         href="https://www.instagram.com/timetravellersgazette/"
         target="_blank"
