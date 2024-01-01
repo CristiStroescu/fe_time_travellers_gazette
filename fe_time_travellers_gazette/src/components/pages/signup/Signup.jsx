7b2907e868c0562d66a4937ed764b085c4c54cb8
CristiStroescu <stroescucristian478@yahoo.com>
2024-01-01 12:59:36 +0200

    Login/Signup - Created the connection with BE for Sign Up

--- fe_time_travellers_gazette/src/components/pages/signup/Signup.jsx
+++ fe_time_travellers_gazette/src/components/pages/signup/Signup.jsx
@@ -1,9 +1,28 @@
+import { useState } from "react";
 import { LOGIN } from "../../../constants/routes";
 import Logo from "../../common/logo/Logo";
 import styles from "./Signup.module.css";
 import { NavLink } from "react-router-dom";
 
+import axios from "axios";
+
 function Signup() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function save(event) {
+    event.preventDefault();
+    try {
+      await axios.post("http://localhost:8080/user/save", {
+        username: username,
+        password: password,
+      });
+      alert("User reg. success");
+    } catch (error) {
+      alert(error);
+    }
+  }
+
   return (
     <>
       <Logo />
@@ -17,6 +36,10 @@ function Signup() {
             className={styles["input-field"]}
             placeholder="Username"
             autoComplete="off"
+            value={username}
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
             required
           />
         </div>
@@ -26,11 +49,17 @@ function Signup() {
             className={styles["input-field"]}
             placeholder="Password"
             autoComplete="off"
+            value={password}
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
             required
           />
         </div>
         <div className={styles["input-submit"]}>
-          <button className={styles["submit-btn"]}>Sign Up</button>
+          <button type="submit" className={styles["submit-btn"]} onClick={save}>
+            Sign Up
+          </button>
         </div>
         <div className={styles["log-in-link"]}>
           <p>
