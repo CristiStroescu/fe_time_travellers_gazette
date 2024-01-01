30f353fc3a9c2801267b958bb3bce3aa3700f574
CristiStroescu <stroescucristian478@yahoo.com>
2024-01-01 13:34:07 +0200

    Login/Signup - Created the connection with BE for Log In

--- fe_time_travellers_gazette/src/components/pages/login/Login.jsx
+++ fe_time_travellers_gazette/src/components/pages/login/Login.jsx
@@ -1,9 +1,53 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import styles from "./Login.module.css";
 import { SIGNUP } from "../../../constants/routes";
 import Logo from "../../common/logo/Logo";
+import axios from "axios";
+import { useState } from "react";
 
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  async function login(event) {
+    event.preventDefault();
+    try {
+      await axios
+        .post("http://localhost:8080/user/login", {
+          username: username,
+          password: password,
+        })
+        .then(
+          (res) => {
+            console.log(res.data);
+            switch (res.data.message) {
+              case "Username does not exist":
+                alert("Username does not exist");
+                break;
+              case "Login Failed":
+                alert("Login Failed");
+                break;
+              case "Password not match":
+                alert("Password not match");
+                break;
+              case "Login Success":
+                navigate("/");
+                break;
+              default:
+                alert("Unknown error");
+                break;
+            }
+          },
+          (fail) => {
+            console.error(fail);
+          }
+        );
+    } catch (err) {
+      alert(err);
+    }
+  }
+
   return (
     <>
       <Logo />
@@ -17,6 +61,10 @@ function Login() {
             className={styles["input-field"]}
             placeholder="Username"
             autoComplete="off"
+            value={username}
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
             required
           />
         </div>
@@ -25,6 +73,10 @@ function Login() {
             type="password"
             className={styles["input-field"]}
             placeholder="Password"
+            value={password}
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
             autoComplete="off"
             required
           />
@@ -36,9 +88,11 @@ function Login() {
           </section>
         </div>
         <div className={styles["input-submit"]}>
-          <button className={styles["submit-btn"]}>Log In</button>
+          <button className={styles["submit-btn"]} onClick={login}>
+            Log In
+          </button>
         </div>
-        <div className={styles["sign-up-link"]}>
+        <div type="submit" className={styles["sign-up-link"]}>
           <p>
             Don't have an account? <NavLink to={SIGNUP}>Sign Up</NavLink>
           </p>
