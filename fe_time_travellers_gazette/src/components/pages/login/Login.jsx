5cd6a5d4e075c1c9299a284fd17b96f4e3ec43c6
CristiStroescu <stroescucristian478@yahoo.com>
2024-01-02 15:44:13 +0200

    Login/Signup - Created the functionality for 'Remember me' checkbox

--- fe_time_travellers_gazette/src/components/pages/login/Login.jsx
+++ fe_time_travellers_gazette/src/components/pages/login/Login.jsx
@@ -2,13 +2,13 @@ import { NavLink, useNavigate } from "react-router-dom";
 import styles from "./Login.module.css";
 import { HOMEPAGE, SIGNUP } from "../../../constants/routes";
 import Logo from "../../common/logo/Logo";
-import axios from "axios";
 import { useState } from "react";
 import { userService } from "../../../services/userService";
 
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [rememberMe, setRememberMe] = useState(false);
   const navigate = useNavigate();
 
   const handleUsernameChange = (event) => {
@@ -19,10 +19,13 @@ function Login() {
     setPassword(event.target.value);
   };
 
+  const handleCheckboxChange = () => {
+    rememberMe ? setRememberMe(false) : setRememberMe(true);
+  };
+
   const handleLoginUser = async () => {
     if (username != "" && password != "") {
       const res = await userService.loginUser(username, password);
-      console.log(res.data.message);
       switch (res.data.message) {
         case "Username does not exist":
           alert("Username does not exist");
@@ -34,7 +37,10 @@ function Login() {
           alert("Password not match");
           break;
         case "Login Success":
-          sessionStorage.setItem("username", username);
+          console.log("rm:", rememberMe);
+          rememberMe
+            ? localStorage.setItem("username", username)
+            : sessionStorage.setItem("username", username);
           navigate(HOMEPAGE);
           break;
         default:
@@ -43,44 +49,6 @@ function Login() {
       }
     }
   };
-
-  async function login(event) {
-    event.preventDefault();
-    try {
-      await axios
-        .post("http://localhost:8080/user/login", {
-          username: username,
-          password: password,
-        })
-        .then(
-          (res) => {
-            console.log(res.data);
-            switch (res.data.message) {
-              case "Username does not exist":
-                alert("Username does not exist");
-                break;
-              case "Login Failed":
-                alert("Login Failed");
-                break;
-              case "Password not match":
-                alert("Password not match");
-                break;
-              case "Login Success":
-                navigate("/");
-                break;
-              default:
-                alert("Unknown error");
-                break;
-            }
-          },
-          (fail) => {
-            console.error(fail);
-          }
-        );
-    } catch (err) {
-      alert(err);
-    }
-  }
 
   return (
     <>
@@ -113,7 +81,12 @@ function Login() {
         </div>
         <div className={styles.remember}>
           <section>
-            <input type="checkbox" id="checkbox" className={styles.checkbox} />
+            <input
+              type="checkbox"
+              id="checkbox"
+              className={styles.checkbox}
+              onChange={handleCheckboxChange}
+            />
             <label htmlFor="checkbox">Remember me</label>
           </section>
         </div>
