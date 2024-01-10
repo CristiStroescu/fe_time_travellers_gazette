3fa5812f51fae50b06393610b8d615c88dca5723
CristiStroescu <stroescucristian478@yahoo.com>
2024-01-10 23:49:32 +0200

    Card - Added the link to a Card component

--- fe_time_travellers_gazette/src/components/common/card/Card.jsx
+++ fe_time_travellers_gazette/src/components/common/card/Card.jsx
@@ -1,15 +1,21 @@
 import styles from "./Card.module.css";
 import PropTypes from "prop-types";
-import image from "../../../assets/img/QueenMarie.jpeg";
+import { Link } from "react-router-dom";
 
 function Card(props) {
   console.log(props.imagePath);
   return (
-    <div className={styles.card}>
-      <img src={props.imagePath} />
-      <h2 className={styles["card-title"]}>{props.title}</h2>
-      <p className={styles["card-subtitle"]}>{props.subtitle}</p>
-    </div>
+    <Link to={props.id}>
+      <div className={styles.card}>
+        <img
+          className={styles["card-image"]}
+          src={props.imagePath}
+          alt="image"
+        />
+        <h2 className={styles["card-title"]}>{props.title}</h2>
+        <p className={styles["card-subtitle"]}>{props.subtitle}</p>
+      </div>
+    </Link>
   );
 }
 
