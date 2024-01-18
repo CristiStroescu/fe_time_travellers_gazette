60abfd572dcf9a0e4f1c705f69e01608a1285cf9
CristiStroescu <stroescucristian478@yahoo.com>
2024-01-18 21:36:24 +0200

    fix

--- fe_time_travellers_gazette/src/services/articleService.js
+++ fe_time_travellers_gazette/src/services/articleService.js
@@ -1,7 +1,9 @@
 import { ARTICLES } from "../constants/constants";
 
 export const receiveArticle = (id) => {
-  const article = ARTICLES.find((article) => article.id === id);
+  console.log(id);
+  const article = ARTICLES.find((article) => article.id == id);
+  console.log(article);
 
   return article || null;
 };
